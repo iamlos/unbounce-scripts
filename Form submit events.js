78605428f119bf346a9873cc9a8cb8a18a842a1a
Mark Wainwright <mark@unbounce.com>
2014-04-29 16:14:57 -0700

    Fix jQuery namespacing issues

--- Form submit events.js
+++ Form submit events.js
@@ -1,24 +1,18 @@
-var yourSubmitFunction = function(e){
+var yourSubmitFunction = function(e,$){
   e.preventDefault();
+  
   // Your code goes in here
-
-  alert('Hold on...');
+  alert('Form has been submitted.');
 
   // If your code is asynchronous, call this line as a callback to your code instead
-  //lp.jQuery('form').submit();
+  $('form').submit();
 }
   
-$(document).ready(function(){
-    
+lp.jQuery(function($){
   $('.lp-pom-form .lp-pom-button').unbind('click').bind('click.formSubmit', function(e){
-    yourSubmitFunction(e);
+    yourSubmitFunction(e,$);
   });
-  
   $('form').bind('keypress.formSubmit', function(e) {
-    if(e.which === 13 && e.target.nodeName.toLowerCase() !== 'textarea') yourSubmitFunction(e);
+    if(e.which === 13 && e.target.nodeName.toLowerCase() !== 'textarea') yourSubmitFunction(e,$);
   });
-
-  // Remove a listener
-  //$('.lp-pom-form .lp-pom-button').unbind('click.formSubmit keypress.formSubmit');
-  
 });
