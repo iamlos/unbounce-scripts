4b53c9864ca9c2bb342373419abf4d109cacf4b3
Mark Wainwright <mark@unbounce.com>
2014-06-12 16:32:03 -0700

    Add horizontal form support for responsive designs; object-ize

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,7 +21,5 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
-
   grunt.registerTask('default', ['jshint']);
-
 };
--- scripts/horizontal-forms.js
+++ scripts/horizontal-forms.js
@@ -1,35 +1,49 @@
-lp.jQuery(function($) {
+function HorizontalForm() {
 
   // Spacing between fields, in pixels:
-  var spacing = 20;
+  this.spacing = 20;
 
   // Options for the submit button placement are 'inline', 'newline' or 'manual':
   // Manual means the script won't touch the submit button - it can be positioned in the builder.
-  var submitPlacement = 'inline';
+  this.submitPlacement = 'inline';
 
   // There's usually no need to change anything beyond here.
 
   // Grab all form fields and the submit button
-  var form = $('.lp-pom-form').eq(0);
-  var fields = $('div.lp-pom-form-field');
-  var button = $('.lp-pom-form .lp-pom-button').eq(0);
-  if ( submitPlacement !== 'manual' ) fields = fields.add(button);
+  this.form = $('.lp-pom-form').eq(0);
+  this.fields = $('div.lp-pom-form-field');
+  this.button = $('.lp-pom-form .lp-pom-button').eq(0);
+  if ( this.submitPlacement !== 'manual' ) this.fields = this.fields.add( this.button );
+  var _this = this;
+
+  // Lay the form out when ready
+  lp.jQuery(function(){
+    _this.layout();
+  });
+
+  // Re-layout when breakpoint changes
+  lp.jQuery(window).resize(function() {
+    _this.layout();
+  });
+}
+
+HorizontalForm.prototype.layout = function() {
 
   // Work out the horizontal offset of fields
-  var width = fields.eq(0).width() + spacing;
+  var width = this.fields.eq(0).width() + this.spacing;
 
   // Record the first field's height as tallest for now
-  var tallest = fields.eq(0).height();
+  var tallest = this.fields.eq(0).height();
 
   // Get the maximum width the form can be
-  var maxWidth = $('.lp-pom-block').eq(0).width() - form.position().left;
+  var maxWidth = $('.lp-pom-block').eq(0).width() - this.form.position().left;
 
   var top = 0;
   var left = 0;
 
   // Loop through each field
-  for ( var i = 1 ; i < fields.length ; i ++ ) {
-    field = fields.eq(i);
+  for ( var i = 1 ; i < this.fields.length ; i ++ ) {
+    field = this.fields.eq(i);
 
     // Add a new 'width' amount to last field's left position for placement later
     left = left + width;
@@ -38,7 +52,7 @@ lp.jQuery(function($) {
     if ( left + width > maxWidth ) {
 
       // If so, start a new line under the tallest field on current line
-      top = top + tallest + spacing;
+      top = top + tallest + this.spacing;
       left = 0;
 
       // Reset tallest to current field, for now
@@ -49,7 +63,7 @@ lp.jQuery(function($) {
 
     if ( field.is('.lp-pom-button') ) {
 
-      if ( submitPlacement === 'newline' ) {
+      if ( this.submitPlacement === 'newline' ) {
         // Put button on new line
         thisTop = top + tallest + spacing;
         left = 0;
@@ -76,10 +90,12 @@ lp.jQuery(function($) {
   }
 
   // Resize the form container so error boxes are positioned better
-  form.css({
+  this.form.css({
     'right': '0',
     'width': 'auto',
     'height': top + tallest +'px'
   });
 
-});
+};
+
+new HorizontalForm();
