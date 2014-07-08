5ca95847d5e95a4a4f61f3dcbb363f9a48d0132a
Mark Wainwright <mark@unbounce.com>
2014-07-08 15:57:05 -0700

    Fix AOL and add other free webmail domains

--- scripts/validation-free-webmails.js
+++ scripts/validation-free-webmails.js
@@ -6,7 +6,7 @@
 
     // Define the rule and apply it to the field
     $.validator.addMethod('notWebmail', function(value) {
-      return !/\@(gmail|googlemail|hotmail|yahoo|aol.co)\./.test(value);
+      return !/\@(gmail|googlemail|hotmail|live|msn|outlook|yahoo|ymail|aol)\./.test(value);
     }, message);
     module.lp.form.data.validationRules[field].notWebmail = true;
 
