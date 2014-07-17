969587355f684613b1b030d859c9237df1533a9e
Mark Wainwright <mark@unbounce.com>
2014-07-17 12:06:53 -0700

    Add script for validating Canadian postal codes

---
+++ scripts/validation-canada-postal-code.js
@@ -0,0 +1,13 @@
+lp.jQuery(function($) {
+
+  // Config
+  var field = 'postal_code';
+  var message = 'Please enter a valid Canadian postal code';
+
+  // Define the rule and apply it to the field
+  $.validator.addMethod('canadianPostalCode', function(value) {
+    return /^([a-zA-Z]\d[a-zA-z]\s?\d[a-zA-Z]\d)$/.test(value);
+  }, message);
+  module.lp.form.data.validationRules[field].canadianPostalCode = true;
+
+});
