2f2ec02dab5222cbba88a20e5d5e51bb1f70615d
Mark Wainwright <mark@unbounce.com>
2014-10-03 12:11:30 -0700

    Prevent smooth scroll from being run twice

--- scripts/smooth-scroll.js
+++ scripts/smooth-scroll.js
@@ -1,10 +1,11 @@
-lp.jQuery(function($){
+lp.jQuery(function($) {
 
   // The speed of the scroll in milliseconds
   var speed = 1000;
 
   // Find links that are #anchors and scroll to them
-  $('a[href^=#]').not('.lp-pom-form .lp-pom-button').click(function(event) {
+  // Unbind existing handler first in case the script has already been run
+  $('a[href^=#]').not('.lp-pom-form .lp-pom-button').unbind('click.smoothScroll').bind('click.smoothScroll', function(event) {
     event.preventDefault();
     $('html, body').animate({ scrollTop: $( $(this).attr('href') ).offset().top }, speed);
   });
